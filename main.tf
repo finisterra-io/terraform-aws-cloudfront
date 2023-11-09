7d19891365b059eb397968603aa224099c6c6c5e
xneyder <xneyder@gmail.com>
2023-11-09 12:46:59 -0500

    Fix

--- main.tf
+++ main.tf
@@ -166,7 +166,7 @@ resource "aws_cloudfront_distribution" "this" {
         iterator = l
 
         content {
-          event_type   = f.value.event_type
+          event_type   = l.value.event_type
           lambda_arn   = l.value.lambda_arn
           include_body = lookup(l.value, "include_body", null)
         }
@@ -230,7 +230,7 @@ resource "aws_cloudfront_distribution" "this" {
         iterator = l
 
         content {
-          event_type   = f.value.event_type
+          event_type   = lookup(l.value, "event_type", null)
           lambda_arn   = l.value.lambda_arn
           include_body = lookup(l.value, "include_body", null)
         }
