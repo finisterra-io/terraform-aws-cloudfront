0f291f1f294386a788c7b856337781a028420a19
xneyder <xneyder@gmail.com>
2023-12-13 22:52:27 -0500

    Fix

--- modules/origin_request_policy/main.tf
+++ modules/origin_request_policy/main.tf
@@ -6,7 +6,7 @@ resource "aws_cloudfront_origin_request_policy" "this" {
   cookies_config {
     cookie_behavior = var.cookie_behavior
     dynamic "cookies" {
-      for_each = lenght(var.cookies) > 0 ? [1] : []
+      for_each = length(var.cookies) > 0 ? [1] : []
       content {
         items = var.cookies
       }
@@ -15,7 +15,7 @@ resource "aws_cloudfront_origin_request_policy" "this" {
   headers_config {
     header_behavior = var.header_behavior
     dynamic "headers" {
-      for_each = lenght(var.headers) > 0 ? [1] : []
+      for_each = length(var.headers) > 0 ? [1] : []
       content {
         items = var.headers
       }
@@ -24,7 +24,7 @@ resource "aws_cloudfront_origin_request_policy" "this" {
   query_strings_config {
     query_string_behavior = var.query_string_behavior
     dynamic "query_strings" {
-      for_each = lenght(var.query_strings) > 0 ? [1] : []
+      for_each = length(var.query_strings) > 0 ? [1] : []
       content {
         items = var.query_strings
       }
